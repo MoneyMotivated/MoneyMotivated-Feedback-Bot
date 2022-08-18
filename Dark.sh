c00bfae78c54e435d2efe2938e2b9573f74ad24e
MKN TG <86054899+MrMKN@users.noreply.github.com>
2022-08-18 11:03:32 +0530

    Create Dark.sh

---
+++ Dark.sh
@@ -0,0 +1,6 @@
+echo "Repo searching...."
+git clone https://github.com/MrMKN/Dark-Owl.git /Dark-Owl            
+cd /Dark-Owl
+pip3 install -U -r requirements.txt
+echo "Bot is starting........"
+python3 owl.py
