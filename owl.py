49ec84e3492b354471ab0cf5948d8865751ec6d2
MoneyMotivated <111167651+MoneyMotivated@users.noreply.github.com>
2022-08-24 10:49:33 -0400

    Update owl.py

--- owl.py
+++ owl.py
@@ -5,7 +5,7 @@ from config import *
 import random
 
 Owl = Client(
-     "Dark_Owl",
+     "MoneyMotivated Staff & Feedback Bot",
      api_id=API_ID,
      api_hash=API_HASH,
      bot_token=BOT_TOKEN
