956a4f841afd0ff4e8d61205bec51d293d6481a3
MoneyMotivated <111167651+MoneyMotivated@users.noreply.github.com>
2022-08-24 10:53:06 -0400

    Update Dark.sh

--- Dark.sh
+++ Dark.sh
@@ -1,5 +1,5 @@
 echo "Repo searching...."
-git clone https://github.com/MrMKN/Dark-Owl.git /Dark-Owl            
+git clone https://github.com/MoneyMotivated/MoneyMotivated-Feedback-Bot.git /MoneyMotivated            
 cd /Dark-Owl
 pip3 install -U -r requirements.txt
 echo "Bot is starting........"
