d71c4c1fb96e0a987cc89f42b96abcc6ac837ba6
MoneyMotivated <111167651+MoneyMotivated@users.noreply.github.com>
2022-11-12 19:51:10 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
 
-ADMIN = int(os.environ.get('ADMIN', ''))
+ADMIN = int(os.environ.get('ADMIN', '5418882619'))
 
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "11344399"))
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "36VMz3kzFSnFHkfgTXr7Wdx9bx5HCF8tSs")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5718362115:AAH7lm6iNkaZivnmHs3KKb_gwsvPTVo09FQ")
 
 PHOTO = os.environ.get("PHOTO", "https://telegra.ph/file/cbee88c58fd6708ddfe90.jpg https://telegra.ph/file/9fffc01c8477044d9c14e.jpg").split()
 
